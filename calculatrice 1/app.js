253e3de34c47b5774c57b1f557f62804464a4ec1
Xel0w <jordan.byrgiel@hotmail.fr>
2022-02-02 22:36:03 +0100

    maj

---
+++ calculatrice 1/app.js
@@ -0,0 +1,33 @@
+let display = document.querySelector("#display");
+let memoire = "";
+let nombreEl = document.querySelectorAll(".number");
+let operatorEl = document.querySelectorAll(".op");
+let computeEl = document.querySelector("#compute");
+let clearTouche = document.querySelector("#clear");
+let total = document.querySelector("#total");
+for (let touche of nombreEl){
+    touche.addEventListener("click", gererTouche);
+}
+for (let op of operatorEl){
+    op.addEventListener("click",operator);
+}
+computeEl.addEventListener("click",function(){   
+    total.innerText = eval(memoire);
+})
+clearTouche.addEventListener("click",function(){
+    memoire = ""
+    display.innerText = memoire;
+    total.innerText = "";
+})
+function operator(){
+    op = this.innerText;
+    memoire = memoire + op
+    display.innerText = memoire;
+}
+function gererTouche(){
+    let touche = this.innerText;
+    if(parseFloat(touche) >= 0 || touche === "."){
+        memoire = memoire + touche.toString();
+        display.innerText = memoire;
+    }
+}
---
+++ calculatrice 2/app.js
@@ -0,0 +1,61 @@
+let display = document.querySelector("#display");
+let affichage = "";
+let precedent = 0;
+let operation = null;
+let memoire;
+let nombreEl = document.querySelectorAll(".number");
+let operatorEl = document.querySelectorAll(".op");
+let computeEl = document.querySelector("#compute");
+let clearTouche = document.querySelector("#clear");
+let total = document.querySelector("#total");
+function gererTouche(){
+    let touche = this.innerText;
+    if(parseFloat(touche) >= 0 || touche === "."){
+        affichage = affichage + touche.toString();
+        display.innerText = affichage;
+    }
+}
+function calculer(nb1, nb2, operation){
+    nb1 = parseFloat(nb1);
+    nb2 = parseFloat(nb2);
+    if(operation === "+") return nb1 + nb2;
+    if(operation === "-") return nb1 - nb2;
+    if(operation === "*") return nb1 * nb2;
+    if(operation === "/", nb2===0) return "SyNtAx eRr0r n00b"
+    if(operation === "/") return nb1 / nb2;
+}
+function operator(){
+    op = this.innerText;
+    switch(op){
+        case "+":
+        case "-":
+        case "/":
+        case "*":
+        precedent = (precedent === 0) ? parseFloat(affichage) : calculer(precedent, parseFloat(affichage), operation);
+        total.innerText = precedent+op;
+        operation = op;
+        affichage = "";
+        break;     
+}
+}
+for (let touche of nombreEl){
+    touche.addEventListener("click", gererTouche);
+}
+for (let op of operatorEl){
+    op.addEventListener("click",operator);
+}
+computeEl.addEventListener("click",function(){   
+    precedent = (precedent === 0) ? parseFloat(affichage) : calculer(precedent, parseFloat(affichage), operation);
+    total.innerText = precedent;
+    affichage = precedent;
+    precedent = 0;
+})
+clearTouche.addEventListener("click",function(){
+    affichage = ""
+    display.innerText = affichage;
+    total.innerText = "";
+})
+
+
+
+
