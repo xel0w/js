e590094fce2441d3f12b5a237b2c15ab9f368ab6
xel0w <jordan.byrgiel@hotmail.fr>
2022-02-02 23:01:15 +0100

    maj

--- calculatrice 1/app.js
+++ calculatrice 1/app.js
@@ -5,6 +5,18 @@ let operatorEl = document.querySelectorAll(".op");
 let computeEl = document.querySelector("#compute");
 let clearTouche = document.querySelector("#clear");
 let total = document.querySelector("#total");
+function gererTouche(){
+    let touche = this.innerText;
+    if(parseFloat(touche) >= 0 || touche === "."){
+        memoire = memoire + touche.toString();
+        display.innerText = memoire;
+    }
+}
+function operator(){
+    op = this.innerText;
+    memoire = memoire + op
+    display.innerText = memoire;
+}
 for (let touche of nombreEl){
     touche.addEventListener("click", gererTouche);
 }
@@ -19,15 +31,3 @@ clearTouche.addEventListener("click",function(){
     display.innerText = memoire;
     total.innerText = "";
 })
-function operator(){
-    op = this.innerText;
-    memoire = memoire + op
-    display.innerText = memoire;
-}
-function gererTouche(){
-    let touche = this.innerText;
-    if(parseFloat(touche) >= 0 || touche === "."){
-        memoire = memoire + touche.toString();
-        display.innerText = memoire;
-    }
-}
