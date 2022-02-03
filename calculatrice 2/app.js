632eca5db5f4382a8c7bde139bcaf04022e16049
xel0w <jordan.byrgiel@hotmail.fr>
2022-02-03 15:24:05 +0100

    maj apres flavien

--- calculatrice 2/app.js
+++ calculatrice 2/app.js
@@ -1,13 +1,17 @@
+//Variables
 let display = document.querySelector("#display");
 let affichage = "";
 let precedent = 0;
 let operation = null;
 let memoire;
-let nombreEl = document.querySelectorAll(".number");
-let operatorEl = document.querySelectorAll(".op");
-let computeEl = document.querySelector("#compute");
+let nombre = document.querySelectorAll(".number");
+let operateur = document.querySelectorAll(".op");
+let compute = document.querySelector("#compute");
 let clearTouche = document.querySelector("#clear");
 let total = document.querySelector("#total");
+
+//Fonction
+
 function gererNombre(){
     let touche = this.innerText;
     if(parseFloat(touche) >= 0 || touche === "."){
@@ -15,15 +19,17 @@ function gererNombre(){
         display.innerText = affichage;
     }
 }
+
 function calculer(nb1, nb2, operation){
     nb1 = parseFloat(nb1);
     nb2 = parseFloat(nb2);
     if(operation === "+") return nb1 + nb2;
     if(operation === "-") return nb1 - nb2;
     if(operation === "*") return nb1 * nb2;
-    if(operation === "/", nb2===0) return "SyNtAx eRr0r n00b"
+    if(operation === "/" & nb2===0) return "SyNtAx eRr0r n00b"
     if(operation === "/") return nb1 / nb2;
 }
+
 function operator(){
     op = this.innerText;
     precedent = (precedent === 0) ? parseFloat(affichage) : calculer(precedent, parseFloat(affichage), operation);
@@ -32,19 +38,25 @@ function operator(){
     affichage = "";  
     display.innerText=affichage;
 }
-for (let touche of nombreEl){
+
+//Event
+
+for (let touche of nombre){
     touche.addEventListener("click", gererNombre);
 }
-for (let op of operatorEl){
+
+for (let op of operateur){
     op.addEventListener("click",operator);
 }
-computeEl.addEventListener("click",function(){   
+
+compute.addEventListener("click",function(){   
     precedent = (precedent === 0) ? parseFloat(affichage) : calculer(precedent, parseFloat(affichage), operation);
     total.innerText = precedent;
     affichage = precedent;
     precedent = 0;
     display.innerText=""
 })
+
 clearTouche.addEventListener("click",nettoyer);
 function nettoyer(){
     affichage = ""
